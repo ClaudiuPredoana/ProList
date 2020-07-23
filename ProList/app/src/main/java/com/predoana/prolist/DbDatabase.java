f3fb32eea552146c5bf44eac64386584df69f79d
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-07-23 16:15:14 +0300

    create the database

---
+++ ProList/app/src/main/java/com/predoana/prolist/DbDatabase.java
@@ -0,0 +1,26 @@
+package com.predoana.prolist;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@@Database(entities = DbTable.class, version = 1)
+public abstract class DbDatabase extends RoomDatabase {
+    public abstract DbDAO dbDAO();
+
+
+    private static DbDatabase dbDatabaseInstance;
+
+
+    public static synchronized DbDatabase getInstance(Context context) {
+
+        if (dbDatabaseInstance == null)
+        {
+
+    dbDatabaseInstance=Room.databaseBuilder(context.getApplicationContext(), DbDatabase.class, "ProList").build();
+    }
+        return dbDatabaseInstance;
+}
+}
