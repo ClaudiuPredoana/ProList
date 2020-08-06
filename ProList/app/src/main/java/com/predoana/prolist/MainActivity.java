02c2340d12098f8ef5dcd8d4354d1b54f53483b5
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-08-06 17:20:25 +0300

    find other views from the UI

--- ProList/app/src/main/java/com/predoana/prolist/MainActivity.java
+++ ProList/app/src/main/java/com/predoana/prolist/MainActivity.java
@@ -1,10 +1,14 @@
 package com.predoana.prolist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +22,11 @@ public class MainActivity extends AppCompatActivity {
                 R.array.item_count_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+        TextView txtCount = findViewById(R.id.txt_count);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        EditText etxtItem = findViewById(R.id.etxt_new_item);
+        Button button = findViewById(R.id.btn_add);
+
     }
 }
