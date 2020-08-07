ac9face2470fb75b0a0e58f0c7acaecf371288b1
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-08-07 12:41:30 +0300

    Create method for add new item

--- ProList/app/src/main/java/com/predoana/prolist/ItemRepository.java
+++ ProList/app/src/main/java/com/predoana/prolist/ItemRepository.java
@@ -1,12 +1,17 @@
 package com.predoana.prolist;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
 public class ItemRepository {
     private Context context;
 
     public ItemRepository(Context context){
         this.context = context.getApplicationContext();
+    }
+
+    public void addNewItem(DbTable dbTable) {
+        AsyncTask.execute(()->DbDatabase.getInstance(context).dbDAO().insertItem(dbTable));
     }
 
 }
