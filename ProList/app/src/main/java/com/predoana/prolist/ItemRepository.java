d3d10fbfd6c98135f40f3b5d3bfe9f2c323490fd
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-08-07 12:42:56 +0300

    Create get all items method

--- ProList/app/src/main/java/com/predoana/prolist/ItemRepository.java
+++ ProList/app/src/main/java/com/predoana/prolist/ItemRepository.java
@@ -3,6 +3,10 @@ package com.predoana.prolist;
 import android.content.Context;
 import android.os.AsyncTask;
 
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
 public class ItemRepository {
     private Context context;
 
@@ -12,6 +16,10 @@ public class ItemRepository {
 
     public void addNewItem(DbTable dbTable) {
         AsyncTask.execute(()->DbDatabase.getInstance(context).dbDAO().insertItem(dbTable));
+    }
+
+    public LiveData<List<DbTable>> getItems() {
+        return DbDatabase.getInstance(context).dbDAO().getAllItems();
     }
 
 }
