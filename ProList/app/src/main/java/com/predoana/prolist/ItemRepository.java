3d7c95bf50b7cada870bcb8e98a8baf6fa17e6fa
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-08-07 12:40:05 +0300

    Create item repository class

---
+++ ProList/app/src/main/java/com/predoana/prolist/ItemRepository.java
@@ -0,0 +1,12 @@
+package com.predoana.prolist;
+
+import android.content.Context;
+
+public class ItemRepository {
+    private Context context;
+
+    public ItemRepository(Context context){
+        this.context = context.getApplicationContext();
+    }
+
+}
